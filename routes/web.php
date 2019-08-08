403b31c9cbe6f73e7a8576eeddef2ae808d68f1c
gwguo <1241212619@qq.com>
2019-08-08 20:44:48 +0800

    debug

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,4 @@
 */
 Route::get('/', function () {
     return view('welcome');
-<<<<<<< HEAD
 });
-Route::get('teacher/login','Teacher\TeacherController@login');
-Route::post('teacher/logindo','Teacher\TeacherController@logindo');
-=======
-});    
->>>>>>> 098b5a3a7f78c54070d968eeb740b594245ccc6f
