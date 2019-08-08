f27687a70c9631de61dda56f2b97b926aa2d0739
gwguo <1241212619@qq.com>
2019-08-08 07:41:32 +0000

    debug

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::prefix('/pay')->group(function(){
-    Route::get('/pay','pay\PayContrller@pay');
-    Route::get('/alipay','alipay\AlipayController@pay');
-});
-Route::prefix('/user')->group(function(){
-    Route::post('/reg','log\LogController@reg');
-    Route::post('/login','log\LogController@login');
-    Route::post('/index','log\LogController@index');
-    Route::post('/sendEmail','log\LogController@sendEmail');
-    Route::post('/code','log\LogController@code');
-    Route::post('/password','log\LogController@password');
-});
-Route::prefix('/send')->group(function(){
-    Route::get('/sendEmail','send\sendEmailController@sendEmail');
-});
