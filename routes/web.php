855dafcee8e40da9463040797fb0558dd4ea16f7
root <root@guowei.localdomain>
2019-08-08 11:47:00 +0000

    debug

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});    
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/','education\EducationController@index');
